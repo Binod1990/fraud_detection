7d7e15e7acfaecfb6616a274c275d21dd0a78301
Binod1990 <binodn714@gmail.com>
2023-10-08 11:09:03 +0530

    adding 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,2 +1,11 @@
 
 print("adding all ML related code here.")
+
+def model1():
+    print("addig code for ML model1")
+
+def model2():
+    print("addig code for ML model2")
+
+def model3():
+    print("addig code for ML model3")
